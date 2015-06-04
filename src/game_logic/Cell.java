fd2ae3e7f1304a39b974f114187859d5db38ef5a
OJIEHb <geiko2012@yandex.ru>
2015-06-04 20:24:58 +0300

    Add Ship and Cell

---
+++ src/game_logic/Cell.java
@@ -0,0 +1,33 @@
+package game_logic;
+
+/**
+ * Created by andrey on 04.06.15.
+ */
+public class Cell {
+    protected int x, y;
+    private Ship ship;
+    private boolean wasFired = false;
+
+    Cell(int x, int y, Ship ship) {
+        this.x = x;
+        this.y = y;
+        this.ship = ship;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setWasFired() {
+        this.wasFired = true;
+    }
+
+    public boolean isFired() {
+        return wasFired;
+    }
+
+    public boolean isShip() {
+        return ship != null;
+    }
+
+}
